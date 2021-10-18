fc6ed5b3ab99e6e65481ccbeab58b943c290bafd
Danny <maguangbin@88.com>
2021-10-18 23:02:19 +0800

    实现 aside 侧边栏切换

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -4,7 +4,7 @@
     <Topnav />
     <!-- 内容区 -->
     <div class="content">
-      <aside>
+      <aside v-if="asideVisible">
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -27,9 +27,15 @@
 </template>
 <script lang="ts">
 import Topnav from '../components/Topnav.vue'
+import { inject, Ref } from 'vue'
 export default {
   components: {
     Topnav
+  },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>('asideVisible')
+    // console.log('Doc aside 获取的 asideVisible 为：' + asideVisible.value)
+    return { asideVisible }
   }
 }
 </script>
