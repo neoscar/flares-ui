7c94ed2e9c2b22b00472fb4483b23f4de4f53053
Danny <maguangbin@88.com>
2021-10-18 19:18:27 +0800

    topnav 样式更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,11 @@
   <div>
     <!-- 导航条 -->
     <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <!-- 内容区 -->
     <div class="banner">
@@ -18,8 +21,22 @@
     </div>
   </div>
 </template>
-<style lang="sass" scoped>
+<style lang="scss" scoped>
 .topnav {
-  border: 1px solid red;
+  background: pink;
+  display: flex;
+  padding: 16px;
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    > li {
+      margin: 0 1em;
+    }
+  }
 }
 </style>
