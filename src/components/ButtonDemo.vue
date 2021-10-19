2c91ef35659b3ab22139eda1e0edae2de23b577d
Danny <maguangbin@88.com>
2021-10-19 19:57:55 +0800

    支持 size 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,17 @@
 <template>
   <div>Button 示例</div>
-  <h1>示例</h1>
+  <h1>示例 1</h1>
   <div>
     <Button>你好</Button>
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
+  </div>
+  <h1>示例 2</h1>
+  <div>
+    <Button size="big">大大大</Button>
+    <Button>普普通</Button>
+    <Button size="small">小小小</Button>
   </div>
 </template>
 
