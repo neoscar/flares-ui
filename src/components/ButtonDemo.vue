253fff499bb99ec0e65fa49de224242898d0a1a6
Danny <maguangbin@88.com>
2021-10-19 16:38:59 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,11 +2,13 @@
   <div>Button 示例</div>
   <h1>示例</h1>
   <div>
-    <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="small">
-      你好
-    </Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
   </div>
 </template>
+
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
