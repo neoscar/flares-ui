696f8f42ea6dc1c2df460ea87dde8f2d0a79efab
Danny <maguangbin@88.com>
2021-10-19 21:18:36 +0800

    Dialog 支持点击关闭

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,19 +1,19 @@
 <template>
   <template v-if="visible">
-    <div class="flares-dialog-overlay"></div>
+    <div class="flares-dialog-overlay" @click="onClickOverlay"></div>
     <div class="flares-dialog-wrapper">
       <div class="flares-dialog">
         <header>
           标题
-          <span class="flares-dialog-close"></span>
+          <span @click="close" class="flares-dialog-close"></span>
         </header>
         <main>
           <p>第一行字</p>
           <p>第二行字</p>
         </main>
         <footer>
-          <Button level="main">OK</Button>
-          <Button>Cancel</Button>
+          <Button level="main" @click="ok">OK</Button>
+          <Button @click="cancel">Cancel</Button>
         </footer>
       </div>
     </div>
@@ -27,10 +27,45 @@ export default {
     visible: {
       type: Boolean,
       default: false
+    },
+    closeOnClickOverlay: {
+      type: Boolean,
+      default: true
+    },
+    ok: {
+      type: Function
+    },
+    cancel: {
+      type: Function
     }
   },
   components: {
     Button
+  },
+  setup(props, context) {
+    const close = () => {
+      context.emit('update:visible', false)
+    }
+    const onClickOverlay = () => {
+      if (props.closeOnClickOverlay) {
+        close()
+      }
+    }
+    const ok = () => {
+      if (props.ok?.() !== false) {
+        close()
+      }
+    }
+    const cancel = () => {
+      context.emit('cancel')
+      close()
+    }
+    return {
+      close,
+      onClickOverlay,
+      ok,
+      cancel
+    }
   }
 }
 </script>
