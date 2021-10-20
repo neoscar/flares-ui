d5a870dd4572da7a8d10840538f1389f432418ef
Danny <maguangbin@88.com>
2021-10-20 10:36:13 +0800

    实现下划线滑动

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="flares-tabs">
-    <div class="flares-tabs-nav">
+    <div class="flares-tabs-nav" ref="container">
       <div
         class="flares-tabs-nav-item"
         v-for="(t, index) in titles"
+        :ref="
+          el => {
+            if (el) navItems[index] = el
+          }
+        "
         @click="select(t)"
         :class="{ selected: t === selected }"
         :key="index"
       >
         {{ t }}
       </div>
-      <div class="gulu-tabs-nav-indicator"></div>
+      <div class="flares-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="flares-tabs-content">
       <component
@@ -25,7 +30,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue'
-import { computed } from 'vue'
+import { computed, ref, onMounted, onUpdated } from 'vue'
 export default {
   props: {
     selected: {
@@ -33,6 +38,22 @@ export default {
     }
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const x = () => {
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]
+      console.log(result)
+      const { width } = result.getBoundingClientRect()
+      indicator.value.style.width = width + 'px'
+      const { left: left1 } = container.value.getBoundingClientRect()
+      const { left: left2 } = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    onMounted(x)
+    onUpdated(x)
     const defaults = context.slots.default()
     defaults.forEach(tag => {
       if (tag.type !== Tab) {
@@ -55,7 +76,10 @@ export default {
       defaults,
       titles,
       current,
-      select
+      select,
+      navItems,
+      indicator,
+      container
     }
   }
 }
@@ -89,6 +113,7 @@ $border-color: #d9d9d9;
       left: 0;
       bottom: -1px;
       width: 100px;
+      transition: all 250ms;
     }
   }
   &-content {
