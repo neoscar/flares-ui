e9fafae9ad4c263b5e70879a16958d666806461c
Danny <maguangbin@88.com>
2021-10-20 11:28:06 +0800

    优化2

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -30,7 +30,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue'
-import { computed, ref, onMounted, onUpdated, watchEffect } from 'vue'
+import { ref, onMounted, watchEffect } from 'vue'
 export default {
   props: {
     selected: {
@@ -41,16 +41,16 @@ export default {
     const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
-    const x = () => {
-      const { width } = selectedItem.value.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const { left: left1 } = container.value.getBoundingClientRect()
-      const { left: left2 } = selectedItem.value.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
+    onMounted(() => {
+      watchEffect(() => {
+        const { width } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const { left: left1 } = container.value.getBoundingClientRect()
+        const { left: left2 } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
 
     const defaults = context.slots.default()
     defaults.forEach(tag => {
