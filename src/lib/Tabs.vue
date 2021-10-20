3fd3c5321bc61e273f8066714b20fb24f588a63e
Danny <maguangbin@88.com>
2021-10-20 11:30:18 +0800

    优化3

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -18,19 +18,14 @@
       <div class="flares-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="flares-tabs-content">
-      <component
-        class="flares-tabs-content-item"
-        :class="{ selected: c.props.title === selected }"
-        v-for="c in defaults"
-        :is="c"
-      />
+      <component :is="current" :key="current.props.title" />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
-import { ref, onMounted, watchEffect } from 'vue'
+import { computed, ref, onMounted, watchEffect } from 'vue'
 export default {
   props: {
     selected: {
@@ -58,6 +53,9 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+    })
     const titles = defaults.map(tag => {
       return tag.props.title
     })
@@ -65,6 +63,7 @@ export default {
       context.emit('update:selected', title)
     }
     return {
+      current,
       defaults,
       titles,
       select,
@@ -109,12 +108,6 @@ $border-color: #d9d9d9;
   }
   &-content {
     padding: 8px 0;
-    &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
